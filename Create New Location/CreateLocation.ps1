c36bd05b1b0cecc94ca72ed7547bcca7c654e1d3
Nikitin Roman <78508681+IronWillDevops@users.noreply.github.com>
2023-02-20 18:29:34 +0200

    Create CreateLocation.ps1

---
+++ Create New Location/CreateLocation.ps1
@@ -0,0 +1,22 @@
+# Задаем имена OU, которые нужно создать
+$sites = "Sites"
+$city = "KHA"
+$descriptioncity="Place in Kharkiv"
+$OUs = @(
+"Workstations",
+"Contacts",
+"Groups",
+"Users"
+)
+# Подключаемся к Active Directory
+Import-Module ActiveDirectory
+
+# Создаем структуру OU в Active Directory
+New-ADOrganizationalUnit -Name "$country" 
+$newOU=New-ADOrganizationalUnit -Name "$city" -Path "OU=$country,DC=itkha,DC=domain" -Description "$descriptioncity" -PassThru
+
+Write-Output $newOU
+
+ForEach ($OU In $OUs) {
+New-ADOrganizationalUnit -Name $OU -Path $newOU
+}
