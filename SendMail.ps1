2f6cb052f92446baf6e9250ae5a71ae1f8c6f05e
Nikitin Roman <78508681+IronWillDevops@users.noreply.github.com>
2023-04-09 02:26:09 +0300

    SendMail.ps1

---
+++ SendMail.ps1
@@ -0,0 +1,30 @@
+param (
+    [Parameter(Mandatory=$true, Position=1)]
+    $To,
+    [Parameter(Mandatory=$true, Position=2)]
+    $Subject,
+    [Parameter(Mandatory=$true, Position=3)]
+    $Body,
+    [Parameter(Mandatory=$false, Position=4)]
+    $Priority
+
+
+)
+if (-not $Priority) {
+    $Priority = "Normal"
+}
+
+$From = "From@exmaple.com"
+$Password = "Password" | ConvertTo-SecureString -AsPlainText -Force
+$SMTPServer = "smtp.example.com"
+$SMTPPort = "587"
+
+$Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
+$Data = $(Get-Date -Format g)
+$ErrorVariable = $null
+Send-MailMessage -From $From -to $To -Subject $Subject -Body $Body  -SmtpServer $SMTPServer -Port $SMTPPort -UseSsl -Credential $Credential -Priority $Priority -Encoding 'UTF8'  -ErrorVariable ErrorVariable
+if ($ErrorVariable) { 
+    Write-Host "Ошибка отправки сообщения $To : $($ErrorVariable[0].Exception.Message)"
+} else {
+    Write-Host "[$Data] Сообщение $To успешно отправлено"
+}
